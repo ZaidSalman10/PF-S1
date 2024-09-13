2a12126ff4d04a2f9796b7d35dd9b4562ed1bdaa
ZaidSalman10 <m.zaidslmaan@gmail.com>
2024-09-13 17:54:44 +0500

    TASK

--- PD2/Name.cpp
+++ PD2/Name.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 main() {
+system ("color 46")
 cout << "##########     ####     ########## ######         " <<endl;
 cout << "#########     ##  ##    ########## ##    ##       " <<endl;
 cout << "      ##     ##    ##       ##     ##     ##      " <<endl;
