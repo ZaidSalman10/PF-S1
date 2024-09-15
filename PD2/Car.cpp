0a93f9177495d4b602470e96f05f2ed33488a276
ZaidSalman10 <m.zaidslmaan@gmail.com>
2024-09-15 19:23:27 +0500

    TASK

---
+++ PD2/Car.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+main() {
+system("color 53"); 
+cout << "                                     " <<endl;
+cout << "               ____________________                                " <<endl;
+cout << "              //         |        \\               " <<endl;
+cout << "             //          |         \\              " <<endl;
+cout << "           //------------------------\\                                   " <<endl;
+cout << "    -----------------------------------------                           " <<endl;
+cout << "  //  __________                 __________  \\           " <<endl;
+cout << " //__|          |               |          |__\\     " <<endl;
+cout << "     |          |_______________|          |            " <<endl;
+cout << "     |          |               |          |        " <<endl;
+cout << "     |__________|               |__________|      " <<endl;
+cout << "                                     " <<endl;
+cout << "                                     " <<endl;
+cout << "                                     " <<endl;
+}
